e6813370e01066f52dd1d59c70ca1e22d64d54b5
cesar.cardona <cesar.cardona@azzule.com>
2026-08-09 13:13:52 -0600

    fix: loop pieces on game over

--- game.js
+++ game.js
@@ -262,6 +262,7 @@ function togglePause() {
 }
 
 function loop(ts) {
+  if (gameOver) return;
   const dt = ts - lastTime;
   lastTime = ts;
   dropAccum += dt;
@@ -271,6 +272,7 @@ function loop(ts) {
       current.y++;
     } else {
       lockPiece();
+      if (gameOver) return;
     }
   }
   draw();
